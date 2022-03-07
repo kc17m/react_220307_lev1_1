92436b47c279a304086549fbde90e6801278f2e2
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-03-07 14:55:34 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount((initial) => {
+      return initial + 1;
+    })
+  }
+
+  function decrement() {
+    setCount((initial) => {
+      return initial - 1;
+    })
+  }
+
+  function res() {
+    setCount((initial) => {
+      return initial = 0;
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {count}</h1>
+      <h1><button onClick={increment}>+</button></h1>
+      <h1><button onClick={decrement}>-</button></h1>
+      <h1><button onClick={res}>Reset</button></h1>
+
+
     </div>
   );
+
 }
 
-export default App;
+export default App
